9ced683216a23409bc3d4cce308dd776d3e38834
Ben Bledsoe <bledsoedesigns@gmail.com>
2017-04-08 12:28:06 -0500

    Creating first component

--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,23 @@
-      // kickoff react app
-      ReactDOM.render(
-        // pass jsx
-        <h1>Calling from seperate jsx file</h1>,
-        // pass dom node
-        document.getElementById('app')
-      );
+// use react library -- take options opject
+// define behavior for our component
+// add and define methods
+var Greeter = React.createClass({
+  // render is the only method required for success
+  // exprects some jsx code that we want to render to dom
+  render: function(){
+    return (
+      <div>
+        <h1>my first react component</h1>
+        <p>Additional component stuffs</p>
+      </div>
+    );
+  }
+});
+
+// kickoff react app
+ReactDOM.render(
+  // pass jsx
+  <Greeter/>,
+  // pass dom node
+  document.getElementById('app')
+);
