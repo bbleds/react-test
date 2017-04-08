4e1bf1d40cbdadd8104a17460769a9431088f908
Ben Bledsoe <bledsoedesigns@gmail.com>
2017-04-08 12:50:27 -0500

    Adding data to components :)

--- public/app.jsx
+++ public/app.jsx
@@ -2,12 +2,15 @@
 // define behavior for our component
 // add and define methods
 var Greeter = React.createClass({
+
   // render is the only method required for success
   // exprects some jsx code that we want to render to dom
   render: function(){
+    var name = this.props.name;
+
     return (
       <div>
-        <h1>my first react component</h1>
+        <h1>Hello {name}, welcome to react world</h1>
         <p>Additional component stuffs</p>
       </div>
     );
@@ -17,7 +20,7 @@ var Greeter = React.createClass({
 // kickoff react app
 ReactDOM.render(
   // pass jsx
-  <Greeter/>,
+  <Greeter name='Ben'/>,
   // pass dom node
   document.getElementById('app')
 );
