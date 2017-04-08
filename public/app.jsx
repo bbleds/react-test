26847c529fd9f9433a1636bdd8a8288d1d0980fa
Ben Bledsoe <bledsoedesigns@gmail.com>
2017-04-08 12:17:26 -0500

    Inital react training app with simple express server

---
+++ public/app.jsx
@@ -0,0 +1,7 @@
+      // kickoff react app
+      ReactDOM.render(
+        // pass jsx
+        <h1>Calling from seperate jsx file</h1>,
+        // pass dom node
+        document.getElementById('app')
+      );
